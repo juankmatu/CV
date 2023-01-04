5dcbec631634c128259a01334616f1d850c0336c
juank <juankdenis@gmail.com>
2023-01-04 20:42:08 -0300

    inicio

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function mostrar() {
+     document.getElementById('id').style.display = 'block';
+     /*oculta */
+     document.getElementById('id').style.display = 'none'
+}
+
+
+function ocultar() {
+     document.getElementById('id').style.display = 'none';
+     /*muetra */
+     document.getElementById('id').style.display = 'block'
+}
